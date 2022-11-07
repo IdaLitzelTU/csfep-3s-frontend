b7d832a3a6a21098bd37cdeef181c5373752edf8
alex karari <karari.alexander@gmail.com>
2022-11-07 22:30:54 +0300

    Modified DynamicRender component.
    Co-authored-by: Denys Sementsov <denys.sementsov@dalberg.com>

--- components/DynamicRender.jsx
+++ components/DynamicRender.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import * as client from "../pages/api/csfep";
 import Grid from "@mui/material/Grid";
@@ -6,89 +6,103 @@ import Stack from "@mui/material/Stack";
 import Tooltip from "@mui/material/Tooltip";
 // import { styled } from "@mui/material/styles";
 
-const DynamicRender = () => {
-  const { data } = useQuery(["model-input"], client.fetchModelInput);
-  const categories = data
-    ? [...new Set(data.input.map((el) => el.category))]
-    : [];
-  const formData = {
-    Forest: [{}],
-  };
-  categories.forEach((key) => {
-    formData[key] = data
-      ? data.input.filter((el) => el.category === key)
-      : [{}];
-  });
-  const Number = ({ defaultValue, name }) => {
-    // console.log(defaultValue);
-    return (
-      <div>
-        <input
-          type="number"
-          step="0.0001"
-          // pattern="[A-Za-z0-9 ]+"
-          key={name}
-          defaultValue={defaultValue}
-          onChange={(e) => console.log(e)}
-          required
-        />
-      </div>
-    );
-  };
-  const Array = ({ defaultValue, name }) => {
-    return (
-      <div>
-        <input
-          key={name}
-          defaultValue={defaultValue}
-          placeholder="Enter array, E.g., [3, 4.7]"
-          required
-          onChange={handleArrays}
-        />
-      </div>
-    );
-  };
+const Number = ({ key, default: defaultValue }) => {
+  // console.log(defaultValue);
+  return (
+    <div>
+      <input
+        type="number"
+        step="0.0001"
+        // pattern="[A-Za-z0-9 ]+"
+        key={key}
+        defaultValue={defaultValue}
+        onChange={(e) => console.log(e)}
+        required
+      />
+    </div>
+  );
+};
+
+const Array = ({ key, default: defaultValue }) => {
   const handleArrays = (e) => {
     const arrayPresent = e.target.value;
   };
 
-  const renderers = {
-    number: <Number />,
-    "array[number]": <Array />,
-  };
+  return (
+    <div>
+      <input
+        key={key}
+        defaultValue={defaultValue}
+        placeholder="Enter array, E.g., [3, 4.7]"
+        required
+        onChange={handleArrays}
+      />
+    </div>
+  );
+};
+
+const renderers = {
+  number: Number,
+  "array[number]": Array,
+};
+
+const DynamicRender = () => {
+  const { data } = useQuery(["model-input"], client.fetchModelInput);
+
+  const [formData, setFormData] = useState(undefined);
+
+  useEffect(() => {
+    if (data) {
+      const categories = data
+        ? [...new Set(data.input.map((el) => el.category))]
+        : [];
+
+      const tempFormData = {};
+
+      categories.forEach((key) => {
+        tempFormData[key] = data
+          ? data.input.filter((el) => el.category === key)
+          : [{}];
+      });
+
+      setFormData(tempFormData);
+    }
+  }, [data]);
 
   return (
     <>
-      <Grid container>
-        {Object.keys(formData).map((key) => {
-          return (
-            <Grid item key={key}>
-              <h3>{key}</h3>
-              {formData[key].map((element) => {
-                const Renderer = () => {
-                  return renderers[element.type];
-                };
-                return (
-                  <Grid item key={element.name}>
-                    <Stack flow="columns">
-                      <div>
-                        <p>{element.display_name}</p>
-                        <Tooltip>
-                          <>{element.description}</>
-                        </Tooltip>
-                      </div>
-                      <Renderer
-                        defaulValue={element.default}
-                        name={element.key}
-                      />
-                    </Stack>
-                  </Grid>
-                );
-              })}
-            </Grid>
-          );
-        })}
-      </Grid>
+      {formData && (
+        <Grid container>
+          {Object.keys(formData).map((key) => {
+            return (
+              <Grid item key={key}>
+                <h3>{key}</h3>
+                {formData[key].map((element) => {
+                  const Renderer = renderers[element.type];
+
+                  console.log(Renderer);
+                  // const Renderer = () => {
+                  //   return renderers[element.type];
+                  // };
+                  return (
+                    <Grid item key={element.name}>
+                      <Stack flow="columns">
+                        <div>
+                          <p>{element.display_name}</p>
+                          <Tooltip>
+                            <>{element.description}</>
+                          </Tooltip>
+                        </div>
+                        <Renderer {...element} />
+                      </Stack>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </>
   );
 };
