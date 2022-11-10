2e49413b9896d29c67d26d35ccf507d2f459d080
Denys Sementsov <denys.sementsov@dalberg.com>
2022-11-10 10:37:51 +0000

    Doing the styling of the dynamic formating

--- components/DynamicRender.jsx
+++ components/DynamicRender.jsx
@@ -1,119 +1,212 @@
-import React, { useEffect, useState } from "react";
-import { useQuery } from "react-query";
-import * as client from "../pages/api/csfep";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-// import TextField from "@mui/material/TextField";
-// import { styled } from "@mui/material/styles";
+import React, { useEffect, useState } from "react"
+import { useQuery } from "react-query"
+import * as client from "../pages/api/csfep"
+import Grid from "@mui/material/Grid"
+import Paper from "@mui/material/Paper"
+import Typography from "@mui/material/Typography"
+import Box from "@mui/material/Box"
+import Divider from "@mui/material/Divider"
+import TextField from "@mui/material/TextField"
 
-const Number = ({ key, default: defaultValue }) => {
+// TODO: let metadata define icon for the group
+// TODO: Input metadata should include
+// label
+// units
+// key
+// name (description)
+// category
+// categoryIcon (from the branding)
+//
+
+const Number = ({ key, default: defaultValue, display_name, ...props }) => {
+  const units = getUnits(display_name)
+  const label = cleanString(display_name)
+
   return (
     <div>
-      <input
+      <TextField
+        label={label}
         type="number"
-        step="0.0001"
+        id={key}
         key={key}
-        defaultValue={defaultValue}
-        onChange={(e) => console.log(e)}
-        required
+        // defaultValue={}
+        style={{ width: "100%" }}
+        placeholder={units}
+        helperText={
+          defaultValue === "None"
+            ? ""
+            : `Default value: ${defaultValue}${units ? units : ""}`
+        }
+        // InputProps={{
+        //   startAdornment: units ? (
+        //     <InputAdornment>{units}</InputAdornment>
+        //   ) : (
+        //     <InputAdornment></InputAdornment>
+        //   ),
+        // }}
       />
     </div>
-  );
-};
+  )
+}
 
-const Array = ({ key, default: defaultValue }) => {
-  const handleArrays = (e) => {
-    const arrayPresent = e.target.value;
-  };
+const Array = ({ key, default: defaultValue, display_name, ...props }) => {
+  const units = getUnits(display_name)
+  const label = cleanString(display_name)
+  const [error, setError] = useState(false)
+  let defaultHelper
+  if (defaultValue !== "None") {
+    defaultHelper = `Default value: ${defaultValue}${units ? units : ""}`
+  }
+
+  function notValidInput(input) {
+    setError(false)
+    const elements = input.target.value.split(",")
+    if (elements.length < 2) {
+      setError(true)
+    }
+    elements.forEach((el) => {
+      if (isNaN(el)) {
+        setError(true)
+      }
+    })
+    return error
+  }
 
   return (
     <div>
-      <input
+      <TextField
+        label={label}
+        error={error}
+        id={key}
         key={key}
-        defaultValue={defaultValue}
-        placeholder="Enter array, E.g., [3, 4.7]"
-        required
-        onChange={handleArrays}
+        // defaultValue={}
+        style={{ width: "100%" }}
+        placeholder={units}
+        helperText={`Input comma separated numbers. ${defaultHelper}`}
+        onChange={(e) => notValidInput(e)}
       />
     </div>
-  );
-};
+  )
+}
 
 const renderers = {
   number: Number,
   "array[number]": Array,
-};
+}
 
 const DynamicRender = () => {
-  const { data } = useQuery(["model-input"], client.fetchModelInput);
+  const { data } = useQuery(["model-input"], client.fetchModelInput)
 
-  const [formData, setFormData] = useState(undefined);
+  const [formData, setFormData] = useState(undefined)
 
   useEffect(() => {
     if (data) {
       const categories = data
         ? [...new Set(data.input.map((el) => el.category))]
-        : [];
+        : []
 
-      const tempFormData = {};
+      const tempFormData = {}
 
       categories.forEach((key) => {
         tempFormData[key] = data
           ? data.input.filter((el) => el.category === key)
-          : [{}];
-      });
+          : [{}]
+      })
 
-      setFormData(tempFormData);
+      setFormData(tempFormData)
     }
-  }, [data]);
+  }, [data])
 
   return (
     <>
       {formData && (
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="stretch"
+        <Paper
+          variant="outlined"
+          elevation={12}
+          style={{
+            padding: "5rem",
+            backgroundColor: "whitesmoke",
+          }}
         >
-          {Object.keys(formData).map((key) => {
-            return (
-              <Grid item key={key}>
-                <Typography
-                  variant="h5"
-                  style={{ color: "#005B36" }}
-                  gutterBottom
-                >
-                  {key}
-                </Typography>
-                <Paper variant="outlined" sx={{ m: 1 }}>
-                  {formData[key].map((element) => {
-                    const Renderer = renderers[element.type];
-                    return (
-                      <Grid item key={element.name}>
-                        <Stack direction="row" spacing={2}>
-                          <item>{element.display_name}</item>
-                          <Renderer {...element} />
-                        </Stack>
-                        <Stack direction="row" spacing={2}>
-                          <Typography
-                            sx={{ fontStyle: "italic", fontSize: 12 }}
+          <Box component="form" noValidate autoComplete="off">
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="stretch"
+            >
+              {Object.keys(formData).map((key) => {
+                return (
+                  <>
+                    <Grid item key={key} style={{ paddingBottom: "24px" }}>
+                      <Divider textAlign="left">
+                        <Typography
+                          variant="h5"
+                          style={{ color: "#005B36" }}
+                          gutterBottom
+                        >
+                          {key}
+                        </Typography>
+                      </Divider>
+                    </Grid>
+
+                    {formData[key].map((element) => {
+                      const Renderer = renderers[element.type]
+                      return (
+                        <Grid
+                          item
+                          key={element.name}
+                          style={{ paddingBottom: "24px" }}
+                        >
+                          <Grid
+                            container
+                            direction="row"
+                            spacing={2}
+                            columns={5}
+                            alignItems="stretch"
                           >
-                            {element.description}
-                          </Typography>
-                        </Stack>
-                      </Grid>
-                    );
-                  })}
-                </Paper>
-              </Grid>
-            );
-          })}
-        </Grid>
+                            <Grid item xs={3} style={{}}>
+                              <div
+                                style={{
+                                  height: "100%",
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                              >
+                                <Typography>{element.description}</Typography>
+                              </div>
+                            </Grid>
+                            <Grid item xs={2}>
+                              <Renderer {...element} />
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      )
+                    })}
+                  </>
+                )
+              })}
+            </Grid>
+          </Box>
+        </Paper>
       )}
     </>
-  );
-};
-export default DynamicRender;
+  )
+}
+export default DynamicRender
+
+function getUnits(string) {
+  let units
+  try {
+    const [left, right] = string.split("(")
+    const rest = right.split(")")
+    units = rest[0]
+  } catch {
+    return undefined
+  }
+  return units
+}
+
+function cleanString(string) {
+  return string.split("(")[0]
+}
