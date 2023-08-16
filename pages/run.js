c8bde0f33c66f2ef507a4d1f938c7c8e800ce7eb
sebdodt <mats.dodt@googlemail.com>
2023-08-16 15:55:56 +0000

    added fetchModelOutput() function to run.js, when only dataset ID is passed

--- pages/run.js
+++ pages/run.js
@@ -6,9 +6,12 @@ import DashboardV1 from "../components/dashboards/v1"
 import PropTypes from "prop-types"
 
 export default function Model({ version, dataset }) {
-  const { data } = useQuery(["model-output", version, dataset], () =>
-    client.runModel(version, dataset)
-  )
+  const { data } = useQuery(["model-output", version, dataset], () => {
+    if (typeof dataset === "number") {
+      return client.fetchModelOutput(version, dataset);
+    }
+    return client.runModel(version, dataset);
+  })
 
   const render = ({ data, version, dataset }) => {
     const availableModels = {
@@ -34,5 +37,5 @@ Model.getInitialProps = async ({ query }) => {
 
 Model.propTypes = {
   version: PropTypes.string,
-  dataset: PropTypes.object,
+  dataset: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
 }
